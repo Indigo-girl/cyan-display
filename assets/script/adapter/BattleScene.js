52a2af616d2ab584ecd68faa4d78662d77c89d12
txzm <txzm2018@gmail.com>
2018-08-15 17:49:25 +0800

    a simple bullet demo

--- assets/script/adapter/BattleScene.js
+++ assets/script/adapter/BattleScene.js
@@ -6,7 +6,8 @@
  * @desc [description]
 */
 import ViewEntity from '../../cyan/src/view/ViewEntity';
-
+import LogicWorld from '../../cyan/src/logic/context/LogicWorld';
+import pubfunc from '../../cyan/src/logic/utils/pubfunc';
 
 cc.Class({
     extends: cc.Component,
@@ -21,13 +22,15 @@ cc.Class({
 
     onLoad () {
         const size = this.node.getContentSize();
+        const world = new LogicWorld(size, size, cc.v2(-size.width/2 , -size.height/2), 1);
+        pubfunc.setWorld(world);
         const entity = new ViewEntity({}, 'KM/KM', this.node);
         entity.setHead(cc.v2(1, 0));
         this._entitys.push(entity);
 
         this.node.on(cc.Node.EventType.TOUCH_END, (event)=>{
             const pos = this.node.convertTouchToNodeSpaceAR(event.touch);
-            entity.sm.handleEvent({
+            entity.handleEvent({
                 type: 'moveToPos',
                 detail: pos
             });
