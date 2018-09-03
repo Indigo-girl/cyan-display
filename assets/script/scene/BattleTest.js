35d37d61827f1c33d7fe1e956b5ef2c5a3e7d520
txzm <txzm2018@gmail.com>
2018-09-03 17:46:40 +0800

    add battle btn

--- assets/script/scene/BattleTest.js
+++ assets/script/scene/BattleTest.js
@@ -12,23 +12,37 @@ cc.Class({
         enemyEditBox: cc.EditBox,
         heroEditBox: cc.EditBox,
         tipsLabel: cc.Label,
+        battleBtnNode: cc.Node,
     },
 
     onLoad(){
+        this._defaultTips = '战斗未开始，点击战斗按钮开始战斗';
+        this.tipsLabel.string = this._defaultTips;
         this.node.on(cc.Node.EventType.TOUCH_END, (event) => {
             const pos = this.node.convertTouchToNodeSpaceAR(event.touch);
             if(this._placeFlag){
                 this.addEntity(pos);
                 this._placeFlag = false;
-                this.tipsLabel.string = '';
+                this.tipsLabel.string = this._defaultTips;
             }
         });
+    },
+
+    onBattleReady(){
+        const world = this.node.getComponent('ViewWorld');
+        world.pause = false;
+        this._defaultTips = '战斗进行中';
+        this.tipsLabel.string = this._defaultTips;
+        this.battleBtnNode.active = false;
     },
 
     addEntity(pos){
         const world = this.node.getComponent('ViewWorld');
         let entity = world.addConfigEnetity(this._toPlaceId, this._toPlaceCamp);
         entity.setPosition(pos);
+        if(this._toPlaceCamp===1){
+            entity.setHead(cc.v2(1, 0));
+        }
         return entity;
     },
 
